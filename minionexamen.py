2efc11b8178024ce149d3b43c5b1f52190e9668f
claudiaalozano <clau.lozano.perez@gmail.com>
2021-12-14 11:38:31 +0100

    creo un bucle para si empieza en  mayúscula sumarle un punto a stuart y si empieza en vocal a kevin

--- minionexamen.py
+++ minionexamen.py
@@ -3,3 +3,10 @@ def minion(s):
     scoreStuart = 0
     scoreKevin = 0
     s=s.upper()
+
+    for i in range(len(s)):
+        if s[i] not in vowels:
+            scoreStuart = scoreStuart + (len(s)-i)
+        else: 
+            scoreKevin = scoreKevin + (len(s)-i)
+            
