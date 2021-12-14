67f7ebf2cfe825e376be421b16a056452ed5d7cb
claudiaalozano <clau.lozano.perez@gmail.com>
2021-12-14 10:55:43 +0100

    Posición de las piezas y su icóno para mostrarlas.

--- ajedrezexamen.py
+++ ajedrezexamen.py
@@ -21,3 +21,19 @@ while y == b :
     b = randint(0,2)
 while z == c:
     c = randint(0,2) 
+
+#Posición de las Torres
+# (tableroajedrez[x])[0] = TORRE_NEGRA1
+# (tableroajedrez[y])[1] = TORRE_NEGRA2
+# (tableroajedrez[z])[2] = TORRE_NEGRA3
+# (tableroajedrez[a])[0] = TORRE_BLANCA1
+# (tableroajedrez[b])[1] = TORRE_BLANCA2
+# (tableroajedrez[c])[2] = TORRE_BLANCA3
+
+(tableroajedrez[x])[0] = chr(0x2656)
+(tableroajedrez[y])[1] = chr(0x2656)
+(tableroajedrez[z])[2] = chr(0x2656)
+(tableroajedrez[a])[0] = chr(0x265C)
+(tableroajedrez[b])[1] = chr(0x265C)
+(tableroajedrez[c])[2] = chr(0x265C)
+
