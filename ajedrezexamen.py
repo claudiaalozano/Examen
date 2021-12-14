141736c60420754a21e1efd1ab2957fe5852dcad
claudiaalozano <clau.lozano.perez@gmail.com>
2021-12-14 10:46:57 +0100

    Creo bucle para que las fichas enfrentadas no caigan en la misma casilla

--- ajedrezexamen.py
+++ ajedrezexamen.py
@@ -12,3 +12,12 @@ z = randint (0,2)
 a = randint (0,2)
 b = randint (0,2)
 c = randint (0,2)
+
+#Las fichas enfrentadas no pueden estar en la misma casilla
+
+while x == a:
+    a = randint(0,2)
+while y == b : 
+    b = randint(0,2)
+while z == c:
+    c = randint(0,2) 
