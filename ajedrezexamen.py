04c5fd403f9f6cf25e8bf09cb1fc08ab52f7fa50
claudiaalozano <clau.lozano.perez@gmail.com>
2021-12-14 10:43:27 +0100

    establezco una variable para posicionar las fichas en el tablero con un número random entre el 0-2

--- ajedrezexamen.py
+++ ajedrezexamen.py
@@ -1,7 +1,14 @@
-from random import randit 
+from random import randint, randit 
 
 tableroajedrez= [
     [" " , " " , " "],
     [" " , " " , " "],
     [" " , " " , " "],
 ]
+
+x = randint (0,2)
+y = randint (0,2)
+z = randint (0,2)
+a = randint (0,2)
+b = randint (0,2)
+c = randint (0,2)
