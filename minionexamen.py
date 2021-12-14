90c96d198b43ac7e170f479f359274c049f23a41
claudiaalozano <clau.lozano.perez@gmail.com>
2021-12-14 11:41:50 +0100

    mediante condicionales creo las comparaciones para el ganador del juego

--- minionexamen.py
+++ minionexamen.py
@@ -9,4 +9,12 @@ def minion(s):
             scoreStuart = scoreStuart + (len(s)-i)
         else: 
             scoreKevin = scoreKevin + (len(s)-i)
-            
+    
+    if scoreStuart>scoreKevin:
+        print("Stuart ganador <3, tu puntuación es:" , scoreStuart)
+    elif scoreStuart<scoreKevin:
+        print("Kevin ganador <3 <3, tu puntuacion es:" , scoreKevin)
+    else:
+        print("Empatados :( , volved a jugar.")
+
+        
